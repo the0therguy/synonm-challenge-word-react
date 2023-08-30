2257f2d6f377dbbce921ed0d8525c227154fe30b
the0therguy <nblues399@gmail.com>
2023-08-30 18:15:42 +0600

    initial commit.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
+  const [word, setWord] = useState("")
+  const [synonym, setSynonym] = useState([])
+  const [loading, setLoading] = useState(false)
+  const fetchData = async (word) => {
+    setLoading(true)
+    await fetch(`https://api.datamuse.com/words?rel_syn=${word}`)
+      .then(response=> response.json())
+      .then(setSynonym)
+    setLoading(false)
+  }
+  /*
+   uncontrolled is when we use ref (useRef())
+   controlled is when we use useState(). the state becomes the “single source of truth” for the input elements. Therefore, the App component shown above is a controlled component.
+  */
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    await fetchData(word)
+  }
+
+  const handleWordClick = async (word) => {
+    await fetchData(word)
+    setWord(word)
+  }
+  if (loading) {
+    return <h1 className="text-decoration-style: solid">Loading......</h1>
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="user-input">word: </label>
+          <input id="user-input"
+                 value={word}
+                 onChange={(e)=>setWord(e.target.value)}
+          />
+          <br/>
+          <button className="rounded-none">Submit</button>
+        </form>
+
+        {
+          loading ? <div className="loading">Loading......</div> : <>
+            <ul>
+              {synonym.map((synonym, idx)=>(
+                <li className="word" key={idx} onClick={()=>handleWordClick(synonym.word)}>
+                  {synonym.word}
+                </li>
+              ))}
+            </ul>
+          </>
+        }
+
       </header>
     </div>
   );
